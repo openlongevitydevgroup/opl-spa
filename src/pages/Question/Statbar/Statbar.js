69bff52a9cc45f84ca237394e3110aa05499c077
angelotalay <angelogabrieltalay@gmail.com>
2023-05-25 10:25:57 +0100

    Made statbar responsive

--- src/pages/Question/Statbar/Statbar.js
+++ src/pages/Question/Statbar/Statbar.js
@@ -1,79 +1,44 @@
-import {
-  Box,
-  Paper,
-  ToggleButton,
-  ToggleButtonGroup,
-  Typography,
-  Tooltip,
-} from "@mui/material";
-import TableViewIcon from "@mui/icons-material/TableView";
-import AccountTreeIcon from "@mui/icons-material/AccountTree";
-import FilterAltIcon from "@mui/icons-material/FilterAlt";
-// import FileUploadIcon from '@mui/icons-material/FileUpload';
-import { useDispatch} from "react-redux";
-import {useLoaderData} from 'react-router-dom'
+import { Box, Paper } from "@mui/material";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import { useDispatch, useSelector } from "react-redux";
+import { useLoaderData } from "react-router-dom";
 import { formActions } from "../../../state/Question/questionFormSlice";
-import { questionActions } from "../../../state/Question/questionSlice";
-import styles from "./Statbar.module.css";
+import StatbarButtonGroup from "./StatbarButtonGroup";
 // This functon is for the statbar that allows the user to select whether to view the questions as a table or as a tree view instead
-function TooltipWrapper(props) {
-  const message = props.message;
-  return <Tooltip title={message}>{props.children}</Tooltip>;
-}
+
 function Statbar(props) {
-  const {data:questions} = useLoaderData()
+  const isMobileState = useSelector((state) => state.question.isMobile);
+  const { data: questions } = useLoaderData();
   const dispatch = useDispatch();
   const submitQuestionHandler = () => {
     dispatch(formActions.toggleFormOpen());
-    const element = document.querySelector('.form-title');
-    if(element){
-      element.scrollIntoView({behavior: 'smooth'})
+    const element = document.querySelector(".form-title");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
     }
-
-  };
-  const toggleTreeHandler = () => {
-    dispatch(questionActions.toggleTreeState());
-  };
-  const toggleTableHandler = () => {
-    dispatch(questionActions.toggleListState());
-  };
-  const toggleFilterHandler = () => {
-    dispatch(questionActions.toggleFilter()); 
   };
 
   return (
-    <Box className={styles.statbar}>
-      <Paper>
-        <ToggleButtonGroup>
-          <TooltipWrapper message="Show filter menu">
-            <ToggleButton value="filter" onClick={toggleFilterHandler}>
-              <FilterAltIcon />
-            </ToggleButton>
-          </TooltipWrapper>
-
-
-          <TooltipWrapper message='View questions as flat list'>
-            <ToggleButton value="table" onClick={toggleTableHandler}>
-              <TableViewIcon />
-            </ToggleButton>
-          </TooltipWrapper>
-          <TooltipWrapper message="View questions as hierarchical list">
-          <ToggleButton value="tree" onClick={toggleTreeHandler}>
-            <AccountTreeIcon />
-          </ToggleButton>
-          </TooltipWrapper>
-
-        </ToggleButtonGroup>
-
-        <div className="items-center">
-          <Typography className="pt-1">{questions.length} results</Typography>
+    <Box className="h-full">
+      <Paper className="flex h-12 w-full flex-row justify-evenly">
+        <StatbarButtonGroup />
+        <div className="w-2/4 items-center text-center">
+          <p className="text:sm pt-3 font-bold md:text-base">
+            {questions.length} results
+          </p>
         </div>
-        <button
-          onClick={submitQuestionHandler}
-          className="w-1/6 bg-white rounded-md shadow-lg border border-solid h-full hover:bg-blue-500 active:bg-blue-500 hover:font-bold"
-        >
-          Submit Question
-        </button>
+        <div className="flex flex-col flex-1 h-full w-1/4 ml-8">
+          <button
+            onClick={submitQuestionHandler}
+            className=" h-full rounded-md border border-solid bg-white text-sm shadow-lg hover:bg-blue-500 hover:font-bold active:bg-blue-500 md:text-base"
+          >
+            {isMobileState ? (
+              <FileUploadIcon className="h-full w-full" />
+            ) : (
+              "Submit Question"
+            )}
+          </button>
+        </div>
       </Paper>
     </Box>
   );
---
+++ src/pages/Question/Statbar/StatbarButtonGroup.js
@@ -0,0 +1,47 @@
+import { ToggleButton, Tooltip, ToggleButtonGroup } from "@mui/material";
+import TableViewIcon from "@mui/icons-material/TableView";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import { useDispatch, useSelector } from "react-redux";
+import { questionActions } from "../../../state/Question/questionSlice";
+function TooltipWrapper(props) {
+    const message = props.message;
+    return <Tooltip title={message}>{props.children}</Tooltip>;
+  }
+function StatbarButtonGroup(){
+    const dispatch = useDispatch();
+    const isMobileState = useSelector(state => state.question.isMobile);
+    const toggleTreeHandler = () => {
+        dispatch(questionActions.toggleTreeState());
+      };
+      const toggleTableHandler = () => {
+        dispatch(questionActions.toggleListState());
+      };
+      const toggleFilterHandler = () => {
+        dispatch(questionActions.toggleFilter()); 
+      };
+    
+
+    return(
+        <ToggleButtonGroup size="small" className="w-1/4">
+        <TooltipWrapper message="Show filter menu">
+          <ToggleButton value="filter" onClick={toggleFilterHandler} size="small" className={`${isMobileState ? 'w-1/3' : null}`}>
+            <FilterAltIcon />
+          </ToggleButton>
+        </TooltipWrapper>
+        <TooltipWrapper message='View questions as flat list'>
+          <ToggleButton value="table" onClick={toggleTableHandler} className={`${isMobileState ? 'w-1/3' : null}`}>
+            <TableViewIcon />
+          </ToggleButton>
+        </TooltipWrapper>
+        <TooltipWrapper message="View questions as hierarchical list">
+        <ToggleButton value="tree" onClick={toggleTreeHandler} className={`${isMobileState ? 'w-1/3' : null}`}>
+          <AccountTreeIcon />
+        </ToggleButton>
+        </TooltipWrapper>
+
+      </ToggleButtonGroup>
+    )
+}
+
+export default StatbarButtonGroup;

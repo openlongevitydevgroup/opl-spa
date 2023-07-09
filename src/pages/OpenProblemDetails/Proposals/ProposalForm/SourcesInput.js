0640d8e9e84f6e77801055c609fff257c133d1b4
angelotalay <angelogabrieltalay@gmail.com>
2023-07-09 12:59:39 +0100

    Styling changes.

--- src/pages/OpenProblemDetails/Proposals/ProposalForm/SourcesInput.js
+++ src/pages/OpenProblemDetails/Proposals/ProposalForm/SourcesInput.js
@@ -2,7 +2,17 @@ function SourcesInput(){
     return(
         <div className="sources-input">
             <div className="sources-input title">
-                <h1>Add sources.</h1>
+                <h1 className="text-lg text-theme-blue font-semibold">Add sources:</h1>
+            </div>
+            <div className="form w-8/12 py-4 pb-6">
+                <form action="" className="flex flex-row">
+                    <select name="TEST" id="" placeholder="TEST" className="border border-theme-blue mr-4 px-4">
+                        <option value="Link">Link</option>
+                        <option value="Reference">Reference</option>
+                    </select>
+                    <input type="text" className="border border-theme-blue w-full"></input>
+                </form>
+
             </div>
 
         </div>
--- src/pages/OpenProblemDetails/Proposals/Submissions/Interface/Solutions.js
+++ src/pages/OpenProblemDetails/Proposals/Submissions/Interface/Solutions.js
@@ -2,7 +2,7 @@ import SolutionsComponent from "./SolutionsComponent";
 function Solutions() {
   return (
     <>
-      <h1 className="py-2 pb-4 text-lg text-theme-blue md:text-xl">
+      <h1 className="py-2 pb-4 text-lg text-theme-blue md:text-xl font-semibold">
         Submitted Solutions
       </h1>
       <div className="submitted-list border-y py-2 border-theme-blue">

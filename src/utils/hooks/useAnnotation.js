75b4a2072804486bfbeb6b513bb5651eb6bab778
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-19 15:03:19 +0100

    Added a hook to get annotation data from API.

---
+++ src/utils/hooks/useAnnotation.js
@@ -0,0 +1,15 @@
+import { useEffect,useState } from "react";
+import getRequest from "../functions/getRequest";
+function useAnnotation(endpoint){
+    const [annotation, setAnnotation] = useState("");
+    useEffect(() => {
+        async function getAnnotation(){
+            const data = await getRequest(endpoint); 
+            setAnnotation(data); 
+        }
+        getAnnotation(); 
+    }, [])
+    return annotation;
+};
+
+export default useAnnotation;

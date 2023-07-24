6cfa0763ef60532fef913573c216bd958b6242b8
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-24 16:17:14 +0100

    Added function to set modal content.

---
+++ src/utils/functions/showModal.js
@@ -0,0 +1,13 @@
+import { generalActions } from "../../state/generalStateSlice";
+
+const showModal = (dispatch, title, message, status) => {
+    const modalValue = {
+      title,
+      message,
+      status,
+    };
+    dispatch(generalActions.toggleModal());
+    dispatch(generalActions.setModalContent({ content: modalValue }));
+  };
+
+  export default showModal;

832ab9628c16703ae36dafda347683d6e86cb8bb
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-24 16:14:43 +0100

    Added modal state to general state.

--- src/state/generalStateSlice.js
+++ src/state/generalStateSlice.js
@@ -1,32 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const DEFAULT_STATE = {
-    viewWidth: null,
-    isMobile: false,
-    modal: {
-      isOpen: false, 
-      content: {
-        title: "", 
-        message: "", 
-        status: "",
-      }
-    }
-}
+  viewWidth: null,
+  isMobile: false,
+  modal: {
+    isOpen: false,
+    content: {
+      title: "",
+      message: "",
+      status: "",
+    },
+  },
+};
 
 const reducers = {
-    setWidth(state, actions){
-        state.viewWidth = actions.payload.viewWidth
-      },
-      setIsMobile(state){
-        state.isMobile = state.viewWidth < 450 ? true : false;
-      },
-}
+  setWidth(state, actions) {
+    state.viewWidth = actions.payload.viewWidth;
+  },
+  setIsMobile(state) {
+    state.isMobile = state.viewWidth < 450 ? true : false;
+  },
+  // Generic state for toggling a state. 
+  toggleModal(state){
+    state.modal.isOpen = !state.modal.isOpen; 
+  }, 
+  setModalContent(state,actions){
+    state.modal.content = actions.payload.content;
+  }, 
+  // Generic reducer for setting a state
+  setState(state,actions){
+    state[actions.payload.key] = actions.payload.value;
+  }, 
+  
+};
 
 const generalSlice = createSlice({
-    name: "general",
-    initialState: DEFAULT_STATE,
-    reducers: reducers,
-  });
+  name: "general",
+  initialState: DEFAULT_STATE,
+  reducers: reducers,
+});
 
-export default generalSlice; 
-export const generalActions = generalSlice.actions; 
+export default generalSlice;
+export const generalActions = generalSlice.actions;

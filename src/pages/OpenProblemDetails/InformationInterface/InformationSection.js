f512915aa2aba1c12df96dc3d002bf31ba6ee7d1
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-25 11:12:48 +0100

    Switched related problems to connexted problems and added a description.

--- src/pages/OpenProblemDetails/InformationInterface/InformationSection.js
+++ src/pages/OpenProblemDetails/InformationInterface/InformationSection.js
@@ -1,13 +1,13 @@
 import Classification from "./Sections/Classification";
 import References from "./Sections/References";
-import RelatedProblems from "./Sections/RelatedProblems";
+import ConnectedProblems from "./Sections/ConnectedProblems";
 function InformationSection(props){
     const children = props.children;
     const id = props.id; 
     return(
         <>
         <Classification/>
-        <RelatedProblems children={children}/>
+        <ConnectedProblems children={children}/>
         <References id={id}/>
         </>
     )
---
+++ src/pages/OpenProblemDetails/InformationInterface/Sections/ConnectedProblems.js
@@ -0,0 +1,39 @@
+import InterfaceTemplate from "../InterfaceTemplate";
+import { HashLink } from "react-router-hash-link";
+function ConnectedProblems(props) {
+  const directChildren = props.children;
+  return (
+    <InterfaceTemplate title={"Connected Problems"}>
+      <div className="description pb-2">
+        Each "Connected Problem" represents a focused inquiry, providing valuable
+        insights and a comprehensive understanding of broader problems.
+        Navigate freely to gain a deeper appreciation of complexities and engage
+        with areas of interest.
+      </div>
+      <hr className="border-theme-blue" />
+      <div className="connected-problems-list">
+        {directChildren.length === 0 ? (
+          <p className="pl-2 text-sm font-semibold md:text-base py-4">None</p>
+        ) : (
+          <ul>
+            {directChildren.map((child) => (
+              <li key={child.problem_id}>
+                <HashLink
+                  className=" pl-2 text-sm font-semibold hover:text-theme-blue hover:underline md:text-base"
+                  smooth
+                  to={"../" + child.problem_id + `#${child.problem_id}`}
+                >
+                  <p>
+                    {child.problem_id}: {child.title}
+                  </p>
+                </HashLink>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </InterfaceTemplate>
+  );
+}
+
+export default ConnectedProblems;

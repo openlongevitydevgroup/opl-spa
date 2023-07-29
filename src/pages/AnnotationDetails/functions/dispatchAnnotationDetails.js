3fad8f7da09657710698fda4db5b5855fdf0b43d
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-29 15:17:57 +0100

    Simplified function to just replace empty annotation state object, but to assign a title and id.

--- src/pages/AnnotationDetails/functions/dispatchAnnotationDetails.js
+++ src/pages/AnnotationDetails/functions/dispatchAnnotationDetails.js
@@ -1,38 +1,41 @@
 import { annotationActions } from "../../../state/Annotation/annotationSlice";
 function dispatchAnnotationDetails(dispatch, category, data) {
-  dispatch(annotationActions.setState({key:"annotation", value: category}))
+  dispatch(annotationActions.setState({ key: "annotation", value: category }));
   switch (category) {
     case "theory":
       dispatch(
         annotationActions.setState({
-          key: "theory",
+          key: "details",
           value: {
-            theoryId: data.theory_id,
+            id: data.theory_id,
             title: data.theory_title,
             description: data.theory_description,
             parent: data.theory_id,
           },
         })
       );
+      break;
+
     case "gene":
       dispatch(
         annotationActions.setState({
-          key: "gene",
+          key: "details",
           value: {
-            geneId: data.gene_id,
-            geneName: data.gene_name,
+            id: data.gene_id,
+            title: data.gene_name,
             geneSymbol: data.gene_symbol,
             species: data.gene_species,
           },
         })
       );
+      break;
     case "protein":
       dispatch(
         annotationActions.setState({
-          key: "protein",
+          key: "details",
           value: {
-            proteinId: data.protein_id,
-            proteinName: data.protein_name,
+            id: data.protein_id,
+            title: data.protein_name,
             species: data.species,
           },
         })

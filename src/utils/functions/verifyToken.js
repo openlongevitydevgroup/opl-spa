e62672c1c15c69b7b843809dcdc9db230eeeb22e
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-31 16:30:53 +0100

    na

--- src/utils/functions/verifyToken.js
+++ src/utils/functions/verifyToken.js
@@ -2,11 +2,13 @@ import axios from "axios";
 const verifyToken = async (token) => {
     const secretKey = process.env.REACT_APP_RECAPTCHA_SECRET_KEY
     let apiResponse = []; 
+    console.log(secretKey)
     try {
         const response = await axios.post(process.env.REACT_APP_VERIFY_TOKEN_ENDPOINT, {
             secret: secretKey, 
             response: token,
         }); 
+        console.log(response)
         apiResponse.push(response['data']); 
         return apiResponse 
     }catch (error) {

743d4044239047fc2f21ea2b682f814a399275d1
Angelo Talay <angelogabrieltalay@gmail.com>
2023-08-08 10:07:41 +0100

    Update TextInput.js
    Adding keyword searches for similar problems when the user starts adding a title.

--- src/pages/OpenProblems/Form/Inputs/TextInput.js
+++ src/pages/OpenProblems/Form/Inputs/TextInput.js
@@ -1,10 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 import { formActions } from "../../../../state/Question/questionFormSlice";
 import { formValidationActions } from "../../../../state/Question/formValidationSlice";
 function TextInput(props) {
+  const allProblems = useSelector((state) => state.question.allProblems);
   const formDetailsState = useSelector((state) => state.form.formDetails);
   const isMobileState = useSelector(state => state.question.isMobile);
   const dispatch = useDispatch();
+
+  // State for getting similar problems from input
+  const [similarProblems, setSimilarProblems] = useState([]);
+  useEffect(() => {
+    if(!props.id == "title"){
+      return;
+    }else{
+      const inputTitle = formDetailsState.title.toLowerCase();
+      const similarProblemsFilter = allProblems.filter((problem) => problem.title.toLowerCase().includes(inputTitle) ); 
+      setSimilarProblems(similarProblemsFilter)
+    }
+  }, [formDetailsState.title, similarProblems])
   const onChangeHandler = (e, key) => {
     dispatch(formActions.inputChange({ id: key, value: e.target.value }));
     switch (key){
@@ -17,9 +31,12 @@ function TextInput(props) {
       default:
     } 
   };
+  
+  
 
   return (
-    <div
+    <div className="flex flex-col">
+    <div 
       className={
         
         `${props.id} flex w-full ${isMobileState ? 'flex-col' : 'flex-row'} items-center py-[1.5rem] text-center`
@@ -32,7 +49,20 @@ function TextInput(props) {
         type="text"
         className={`h-fit-content h-auto ${isMobileState ? 'w-full' : 'w-4/5'} rounded border border-slate-500 bg-bg-grey p-2`} required={props.required}
        value={formDetailsState[props.id]} placeholder={props.label} name={props.id}/>
+
     </div>
+    {
+      props.id === "title" &&
+      <div>
+        <ul className="list-disc">
+          {similarProblems.map((problem) => <li>{problem.title}</li>)}
+        </ul>
+
+      </div>
+    }
+
+    </div>
+
   );
 }
 

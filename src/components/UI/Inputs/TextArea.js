572a5fe94fe8f8d9bf534090b1d406b1a98fe4b0
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-09-07 19:17:53 +0100

    Changed TextArea to allow for custom or default setState

--- src/components/UI/Inputs/TextArea.js
+++ src/components/UI/Inputs/TextArea.js
@@ -1,16 +1,23 @@
 import withOnChangeHandler from "../../../utils/hoc/withOnChangeHandler";
 
-
 function textArea(props) {
   return (
     <textarea
-      onChange={props.onChange}
       className={props.className}
       placeholder={props.placeHolder}
     ></textarea>
   );
 }
 
+export function TextAreaNoChangeHandler(props) {
+  return (
+    <textarea
+      className={props.className}
+      placeholder={props.placeHolder}
+      onChange={props.onChange}
+    ></textarea>
+  );
+}
 
-const TextArea = withOnChangeHandler(textArea); 
-export default TextArea;
+const TextAreaChangeHandler = withOnChangeHandler(textArea);
+export default TextAreaChangeHandler;

31e7a909fd4a045f198812d8cce3a53e1cf63f2f
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-09-12 16:48:55 +0100

    Refactored validator.

--- src/utils/functions/validators/pubmedIDValidator.js
+++ src/utils/functions/validators/pubmedIDValidator.js
@@ -1,16 +1,18 @@
 function pubmedValidator(input) {
   // Regular expression to match PubMed ID pattern (numeric value)
-  return new Promise((resolve,reject) => {
+  return new Promise((resolve, reject) => {
     const pubmedIDPattern = /^\d+$/;
     const isValid = pubmedIDPattern.test(input);
-    if(isValid){
-        return resolve(input)
-    }else{
-        return reject("Incorrect pubmed pattern")
+    //If starts with 0 then reject;
+    if (input.startsWith("0")) {
+      return reject("Incorrect pubmed pattern");
     }
-  })
-
-
+    if (isValid) {
+      return resolve(input);
+    } else {
+      return reject("Incorrect pubmed pattern");
+    }
+  });
 }
 
 export default pubmedValidator;

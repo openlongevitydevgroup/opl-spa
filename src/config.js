649726f32d224650ed63742570603fe3e8a6a629
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-09-22 14:40:32 +0100

    Removed env variables.

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export const get_web_api_url = (loc) => {
-    switch (loc.hostname) {
-      case "localhost":
-      case "127.0.0.1":
-        return "http://localhost:5000/api";
-      default:
-        return "https://admin.longevityknowledge.app/api";
-    }
+  switch (loc.hostname) {
+    case "localhost":
+    case "127.0.0.1":
+      return "http://localhost:8000/api";
+    default:
+      return "https://admin.longevityknowledge.app/api";
   }
+};
 
 export const RECAPTCHA_SECRET_KEY = "6LfuXdkmAAAAANrcSZY1LkaUSxDvyBpiSqvmvTO-";
 export const RECAPTCHA_SITE_KEY = "6LfuXdkmAAAAAKEGNxd3xs-wFQjzb4FYVTUB6wO-";
--- src/pages/OpenProblemDetails/functions/submit.js
+++ src/pages/OpenProblemDetails/functions/submit.js
@@ -1,31 +1,44 @@
-import axios from "axios"
 import { detailsActions } from "../../../state/Details/detailsSlice";
-import { postSubmission } from "../../../api/apiSubmissions"
+import postSubmission from "../../../api/apiSubmissions";
 
 const submit = async (data, dispatch) => {
-    const openProblemId = data.openProblem
-    const date = new Date();
-    const requestData = {
-        full_text: data.description,
-        date: date,
-        first_name: data.firstName, 
-        last_name: data.lastName, 
-        affiliation: data.affiliation,
-        open_problem: openProblemId, 
-        submitted_references: JSON.stringify({...data.references}), 
-        is_active: false,
+  const openProblemId = data.openProblem;
+  const date = new Date();
+  const requestData = {
+    full_text: data.description,
+    date: date,
+    first_name: data.firstName,
+    last_name: data.lastName,
+    affiliation: data.affiliation,
+    open_problem: openProblemId,
+    submitted_references: JSON.stringify({ ...data.references }),
+    is_active: false,
+  };
+  try {
+    const response = await postSubmission({
+      data: requestData,
+      problemId: openProblemId,
+    });
+    if (response.status == 201) {
+      dispatch(detailsActions.toggleModalOpen());
+      dispatch(
+        detailsActions.setSubmitState({
+          title: "Post submitted",
+          message: "This post has been submitted for review",
+          status: "success",
+        })
+      );
     }
-    try{
-        const response = await postSubmission({ data: requestData, problemId: openProblemId }); 
-        if(response.status == 201){
-            dispatch(detailsActions.toggleModalOpen())
-            dispatch(detailsActions.setSubmitState({title:"Post submitted" ,message:"This post has been submitted for review", status:"success"}))
-        }
-    }catch(error){
-        dispatch(detailsActions.toggleModalOpen())
-        dispatch(detailsActions.setSubmitState({title:"Unsuccessful submission", message:`${error.message}`, status:"failed"}))
+  } catch (error) {
+    dispatch(detailsActions.toggleModalOpen());
+    dispatch(
+      detailsActions.setSubmitState({
+        title: "Unsuccessful submission",
+        message: `${error.message}`,
+        status: "failed",
+      })
+    );
+  }
+};
 
-    }
-} 
-
-export default submit
+export default submit;

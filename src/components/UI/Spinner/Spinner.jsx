84109aee166f19638975a91a73168ec501e27bd2
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-10-24 19:00:05 +0100

    Refactoring of sidenavigation data manipulation and included loading spinner.

---
+++ src/components/UI/Spinner/Spinner.jsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="animate-spin rounded-full w-10 h-10 bg-gradient-to-tr from-theme-blue-shade to-theme-blue flex items-center justify-center">
+      <div className="h-8 w-8 rounded-full bg-bg-grey"></div>
+    </div>
+  );
+}

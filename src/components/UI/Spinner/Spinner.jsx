1623ec40e74b1fa8fdda83f748a175f79fec7be3
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-10-26 11:39:26 +0100

    Added loading spinner.

--- src/components/UI/Spinner/Spinner.jsx
+++ src/components/UI/Spinner/Spinner.jsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
-    <div className="animate-spin rounded-full w-10 h-10 bg-gradient-to-tr from-theme-blue-shade to-theme-blue flex items-center justify-center">
-      <div className="h-8 w-8 rounded-full bg-bg-grey"></div>
+    <div className="animate-spin rounded-full w-10 h-10 bg-gradient-to-tr from-theme-blue to to-theme-blue-light flex items-center justify-center">
+      <div className="h-7 w-7 rounded-full bg-bg-grey"></div>
     </div>
   );
 }
